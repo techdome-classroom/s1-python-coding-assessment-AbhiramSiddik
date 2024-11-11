e7e142f23c80496240256a10dc546bbc0d44ae02
AbhiramSiddik <abhiorange7@gmail.com>
2024-11-11 14:24:06 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,6 +1,20 @@
 def decode_message( s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+        dp[0][0] = True
+
+        for j in range(1, len(p) + 1):
+                if p[j - 1] == '*':
+                        dp[0][j] = dp[0][j - 1]
+
+        for i in range(1, len(s) + 1):
+                for j in range(1, len(p) + 1):
+                        if p[j - 1] == s[i - 1] or p[j - 1] == '?':
+                                dp[i][j] = dp[i - 1][j - 1]
+                        elif p[j - 1] == '*':
+                                dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
 
 # write your code here
+<<<<<<< Updated upstream
         dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
         dp[0][0] = True
 
@@ -16,3 +30,8 @@ def decode_message( s: str, p: str) -> bool:
                                 dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
   
         return dp[len(s)][len(p)]
+=======
+
+  
+        return dp[len(s)][len(p)]
+>>>>>>> Stashed changes
